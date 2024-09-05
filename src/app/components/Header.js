e6b47bbc95b2e4caadd824339d23ed59baecf5da
tsukagoshi56 <tsukatoshi0506@gmail.com>
2024-09-05 23:06:44 +0900

    Initial portfolio setup

---
+++ src/app/components/Header.js
@@ -0,0 +1,9 @@
+// app/components/Header.js
+export default function Header() {
+    return (
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">塚越のポートフォリオ</h1>
+      </header>
+    );
+  }
+  
